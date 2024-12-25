322ddea2d74ec9794cd9f02e474425cfea406071
Finlay Nelson <Finlay.Nelson@nottingham.ac.uk>
2024-12-25 20:46:15 +0000

    Initialize Git repository and add .gitignore

---
+++ launch_spyder.bat
@@ -0,0 +1,10 @@
+
+@echo off
+REM Activate the Conda environment
+call C:\Users\ezxfn1\anaconda3\Scripts\activate.bat C:\Users\ezxfn1\Projects\Scimitar\scimitarvenv
+
+REM Launch Spyder from the virtual environment
+call spyder
+
+REM Pause to keep the terminal window open (optional)
+pause
