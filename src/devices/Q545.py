33ae8591883187682627cd55f93fcebe27869132
Finlay-Nelson <61199632+Finlay-Nelson@users.noreply.github.com>
2025-01-08 13:12:51 +0000

    Update Q545.py
    Tested but with little documentation

--- src/devices/Q545.py
+++ src/devices/Q545.py
@@ -25,6 +25,9 @@ class Q545():
         self.model = model
         self.serial = serial
         self.pidevice = None
+        self.UpperLim = 6.5
+        self.LowerLim = -6.5
+        self.home = 0.0
 
     def __enter__(self):
         self.pidevice = GCSDevice(self.model)
@@ -32,17 +35,19 @@ class Q545():
             self.pidevice.ConnectUSB(serialnum=self.serial)
             print("Q-545 Connected")
             while True:
-                if not self.isHomed():
-                    prompt = input("Confirm the piezo is safe to home? (y/n)")
+                if not self.isReferenced():
+                    prompt = input("Confirm the piezo is safe to reference? (y/n)")
                     if prompt == "y":
-                        self.home()
-                        print("Q-545 Homed")
+                        self.refernce()
+                        print("Q-545 Referenced")
                     elif prompt == "n":
                         self.__exit__(None,None,None)
                         break
                     else:
                         ValueError("Unrecognised input. Specify y or n") 
                         raise
+                else:
+                    break
         except Exception as e:
             print(f"Error connecting to Q-545: {e}")
             raise
@@ -57,30 +62,41 @@ class Q545():
         self.pidevice.__exit__(None, None, None)
         print("Q-545 Disconnected")
         
-    def isHomed(self):
+    def isReferenced(self):
         servo_status = self.pidevice.read('SVO?')
         return servo_status
 
-    def home(self):
+    def reference(self):
         self.pidevice.SVO(1, 1)
         self.pidevice.send('FNL')
     
-    def setPos(self, target):
-        if target < -6.5 or target > 6.5:
+    def move_absolute(self, target):
+        if target < self.LowerLim or target > self.UpperLim:
             raise ValueError("Target location out of bounds: -6.5 to 6.5mm")
         try:
             self.pidevice.MOV(1, target)
             pitools.waitontarget(self.pidevice, 1)
-            pos = self.getPos()
-            print(pos)
+            pos = self.get_position()
+            print(f"PI Position: {pos}")
         except Exception as e:
             print(e)
+    def move_relative(self, step):
+            pos = self.get_position()
+            if pos + step < self.LowerLim or pos + step > self.UpperLim:
+                raise ValueError("Specified travel would move stage out of range")
+            else:
+                try:
+                    self.pidevice.MVR(1,step)
+                    pitools.waitontarget(self.pidevice, 1)
+                    pos = self.get_position()
+                    print(f"PI Position: {pos}")
+                except Exception as e:
+                    print(e)
 
-    def getPos(self):
+    def get_position(self):
         return self.pidevice.qPOS(1)[1]
     
 if __name__ == "__main__":
-    Q545 = Q545()
-    Q545.setPos(0)
-    Q545.pidevice._settings.keys()
-    
+    with Q545() as Q545:
+        Q545.move_absolute(0.0)
+        Q545.move_relative(1.0)
